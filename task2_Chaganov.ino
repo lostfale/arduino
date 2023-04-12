b938b56a2b78078c0c29f7daca5ef2f2b607afc9
lostfale <64404890+lostfale@users.noreply.github.com>
2023-04-12 15:03:52 +0400

    Update task2_Chaganov.ino

--- task2_Chaganov.ino
+++ task2_Chaganov.ino
@@ -1,32 +1,83 @@
-// Определяем пины светодиодов
-#include <TaskManager.h>
-
+#include <TaskManagerIO.h>
 int ledPins[] = {3, 5, 6, 9, 10, 11};
-int delays[] = {50,100,1000,5000,10000,100000};
-int states[] = {LOW,LOW,LOW,LOW,LOW,LOW};
-
-
+int ledStates[] = {LOW, LOW, LOW, LOW, LOW, LOW};
+ 
 int numLedPins = 6;
-int i = 0;
+ 
 void setup() {
-  
-  
-  for (i; i < numLedPins; i++) {
-    taskManager.scheduleFixedRate(delays[i], [] {
-         if (states[i] == LOW) {
-             states[i] = HIGH;
-      } else {
-        states[i] = LOW;
-      }  
-
-       digitalWrite(ledPins[i], states[i]);
-	  }, TIME_MICROS);    
-    
+ 
+  for (int i = 0; i < numLedPins; i++) {
+    pinMode(ledPins[i], OUTPUT);
   }
  
+  taskManager.scheduleFixedRate(100, [] {
+    if (ledStates[0] == LOW) {
+      ledStates[0] = HIGH;
+      digitalWrite(ledPins[0], HIGH); // Включаем светодиод
+    }
+    else {
+      ledStates[0] = LOW;
+      digitalWrite(ledPins[0], LOW); // Выключаем светодиод
+    }
+  }, TIME_MICROS);
+ 
+  taskManager.scheduleFixedRate(100, [] {
+    if (ledStates[1] == LOW) {
+      ledStates[1] = HIGH;
+      digitalWrite(ledPins[1], HIGH); // Включаем светодиод
+    }
+    else {
+      ledStates[1] = LOW;
+      digitalWrite(ledPins[1], LOW); // Выключаем светодиод
+    }
+  }, TIME_MICROS);
+ 
+  taskManager.scheduleFixedRate(100, [] {
+    if (ledStates[2] == LOW) {
+      ledStates[2] = HIGH;
+      digitalWrite(ledPins[2], HIGH); // Включаем светодиод
+    }
+    else {
+      ledStates[2] = LOW;
+      digitalWrite(ledPins[2], LOW); // Выключаем светодиод
+    }
+  }, TIME_MICROS);
+ 
+  taskManager.scheduleFixedRate(100, [] {
+    if (ledStates[3] == LOW) {
+      ledStates[3] = HIGH;
+      digitalWrite(ledPins[3], HIGH); // Включаем светодиод
+    }
+    else {
+      ledStates[3] = LOW;
+      digitalWrite(ledPins[3], LOW); // Выключаем светодиод
+    }
+  }, TIME_MICROS);
+ 
+  taskManager.scheduleFixedRate(100, [] {
+    if (ledStates[4] == LOW) {
+      ledStates[4] = HIGH;
+      digitalWrite(ledPins[4], HIGH); // Включаем светодиод
+    }
+    else {
+      ledStates[4] = LOW;
+      digitalWrite(ledPins[4], LOW); // Выключаем светодиод
+    }
+  }, TIME_MICROS);
+ 
+  taskManager.scheduleFixedRate(100, [] {
+    if (ledStates[5] == LOW) {
+      ledStates[5] = HIGH;
+      digitalWrite(ledPins[5], HIGH); // Включаем светодиод
+    }
+    else {
+      ledStates[5] = LOW;
+      digitalWrite(ledPins[5], LOW); // Выключаем светодиод
+    }
+  }, TIME_MICROS);
 }
-
+ 
 void loop() {
-  // Мигаем светодиодами по очереди
-    taskManager.runLoop();
+ 
+  taskManager.runLoop();
 }
